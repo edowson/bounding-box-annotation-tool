0edffa251e4c7269866331c8fdd81b768d68f2c4
Elvis Dowson <elvis.dowson@gmail.com>
2020-06-15 11:43:41 +0400

    process.py: Fix name of validation file.
    Signed-off-by: Elvis Dowson <elvis.dowson@gmail.com>

--- process.py
+++ process.py
@@ -2,29 +2,29 @@ import glob, os
 
 # ============================== Config ========================================
 
-DATA_PATH       = 'data/obj/'			# Directory where the data will reside, relative to 'darknet.exe'
-TEST_PERCENTAGE = 30				# Test Image Percentage
-IMAGE_FORMAT    = 'jpg'				# Image Format of your data [jpg / png / jpeg]
+DATA_PATH             = 'data/obj/'      # Directory where the data will reside, relative to 'darknet.exe'
+VALIDATION_PERCENTAGE = 30               # Validation Image Percentage
+IMAGE_FORMAT          = 'jpg'            # Image Format of your data [jpg / png / jpeg]
 
 # ============================== Config ========================================
 
 
 def process():
-	# Create and/or truncate train.txt and test.txt
-	file_train = open('train.txt', 'w')
-	file_test = open('test.txt', 'w')
+	# Create and/or truncate train.txt and val.txt
+	file_train      = open('train.txt', 'w')
+	file_validation = open('val.txt'  , 'w')
 
-	# Populate train.txt and test.txt
+	# Populate train.txt and val.txt
 	counter = 1
-	index_test = round(100 / TEST_PERCENTAGE)
+	index_validation = round(100 / VALIDATION_PERCENTAGE)
 	filesPresent = False
 
 	for pathAndFilename in glob.iglob(os.path.join(DATA_PATH, str("*." + IMAGE_FORMAT) )):
 		title, ext = os.path.splitext(os.path.basename(pathAndFilename))
 		filesPresent = True
-		if counter == index_test:
+		if counter == index_validation:
 			counter = 1
-			file_test.write(DATA_PATH + title + '.' + IMAGE_FORMAT + "\n")
+			file_validation.write(DATA_PATH + title + '.' + IMAGE_FORMAT + "\n")
 		else:
 			file_train.write(DATA_PATH + title + '.' + IMAGE_FORMAT + "\n")
 			counter = counter + 1
